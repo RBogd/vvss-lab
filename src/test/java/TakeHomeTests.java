690311c75319c55a84c6375f54a3941510eae15c
viktorashi <ioanvictorstan@gmail.com>
2025-04-30 17:25:53 +0300

    atat e nevoie pana la urma

---
+++ src/test/java/TakeHomeTests.java
@@ -0,0 +1,16 @@
+package test;
+import domain.Student;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import repository.StudentXMLRepository;
+import service.Service;
+import validation.StudentValidator;
+import validation.Validator;
+import test.GradeIntegrationTest;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TakeHomeTests extends GradeIntegrationTest {
+}
